2bbd2d052745a38631b9f9c90c66fdeb2cfc76f5
Alexalopezm <alexalopez12.al@gmail.com>
2023-09-04 22:49:47 -0600

    Version final del primer script

--- verificador.sh
+++ verificador.sh
@@ -2,8 +2,8 @@
 
 # Se comprueba que se proporciona un argumento llamado enunciado
 
-if [$# -ne 1]; then
-	echo "Uso: $0 <enunciado.pdf>"
+if [ $# -ne 1 ]; then
+	echo "Uso: $0 <nombre_archivo>"
 	exit 1
 fi
 
@@ -11,7 +11,8 @@ fi
 name="$1"
 
 # Verificar si el archivo existe
-if [ -e "$file_name" ]; then
+if [ -e "$name" ]; then
+	echo "EL archivo '$name' existe."
 	# Obtener los permisos del archivo
 	permissions=$(stat -c "%A" "$name")
 
@@ -24,30 +25,73 @@ if [ -e "$file_name" ]; then
 		
 		# Compara los permisos de lectura del usuario
 		if [ "${user_perms:0:1}" == "r" ]; then
-			echo "El usuario tiene permisos de lectura"
+			echo "El usuario tiene permisos de lectura."
 		else
-			echo "El usuario no tiene permisos de lectura"
+			echo "El usuario no tiene permisos de lectura."
 		fi
 
 		# Compara los permisos de escritura del usuario
 		if [ "${user_perms:1:1}" == "w" ]; then
-			echo "El grupo tiene permisos de escritura"
+			echo "El usuario tiene permisos de escritura."
 		else
-			echo "El grupo no tiene permisos de escritura"
+			echo "El usuario no tiene permisos de escritura."
 		fi
 
 		# Compara los permisos de ejecutar del usuario
 		if [ "${user_perms:2:1}" == "x" ]; then
-			echo "El grupo tiene permisos de ejecutar"
+			echo "El usuario tiene permisos de ejecutar."
 		else
-			echo "El grupo no tiene permisos de ejecutar"
+			echo "El usuario no tiene permisos de ejecutar."
 		fi
+
+		# Compara los permisos de lectura del usuario
+		if [ "${group_perms:0:1}" == "r" ]; then
+			echo "El grupo tiene permisos de lectura."
+		else
+			echo "El grupo no tiene permisos de lectura."
+		fi
+
+		# Compara los permisos de escritura del usuario
+		if [ "${group_perms:1:1}" == "w" ]; then
+			echo "El grupo tiene permisos de escritura."
+		else
+			echo "El grupo no tiene permisos de escritura."
+		fi
+
+		# Compara los permisos de ejecutar del usuario
+		if [ "${group_perms:2:1}" == "x" ]; then
+			echo "El grupo tiene permisos de ejecutar."
+		else
+			echo "El grupo no tiene permisos de ejecutar."
+		fi
+
+		# Compara los permisos de lectura del usuario
+		if [ "${others_perms:0:1}" == "r" ]; then
+			echo "Los otros usuarios tienen permisos de lectura."
+		else
+			echo "Los otros usuarios no tienen permisos de lectura."
+		fi
+
+		# Compara los permisos de escritura del usuario
+		if [ "${others_perms:1:1}" == "w" ]; then
+			echo "Los otros usuarios tienen permisos de escritura."
+		else
+			echo "Los otros usuarios no tienen permisos de escritura."
+		fi
+
+		# Compara los permisos de ejecutar del usuario
+		if [ "${others_perms:2:1}" == "x" ]; then
+			echo "Los otros usuarios tienen permisos de ejecutar."
+		else
+			echo "Los otros usuarios no tienen permisos de ejecutar."
+		fi
+
 	}
 
 	# Se llama a la funcion
 	get_permissions_verbose "$permissions"
 else
-	echo "El archivo '$enunciado' no existe."
+	echo "El archivo '$name' no existe."
 	exit 2
 fi
 
